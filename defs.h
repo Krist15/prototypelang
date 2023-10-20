78343b17bedd432ea128526a9ea81f6184df8728
Christopher <127637098+Krisstopher15@users.noreply.github.com>
2023-10-20 08:22:19 -0400

    feat: define global variables

--- defs.h
+++ defs.h
@@ -1,8 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+// token structure
 struct Token {
     int token;
     int intvalue;
 };
 
+//tokens
 enum {
     T_PLUS, T_MINUS, T_STAR, T_SLASH, T_INTLIT
 };
