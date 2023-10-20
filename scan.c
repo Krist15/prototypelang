23a49d2e2fddce4cdd85239796693fd80e35d5c1
Christopher <127637098+Krisstopher15@users.noreply.github.com>
2023-10-20 08:25:35 -0400

    feat: add scanner and declare scan method

---
+++ scan.c
@@ -0,0 +1,93 @@
+#include "defs.h"
+#include "data.h"
+#include "decl.h"
+
+static int chrpos(char *s, int c) {
+    char *p;
+
+    p = strchr(s, c);
+    return (p ? p - s : -1);
+}
+
+static int next(void) {
+    int c;
+
+    if (Putback) {          
+        c = Putback;        
+        Putback = 0;
+        return c;
+    }
+
+    c = fgetc(Infile);       
+
+    if (c == '\n') {
+        Line++;             
+    }
+        return c;
+}
+
+
+static void putback(int c) {
+    Putback = c;
+}
+
+static int skip_whitespace(void) {
+    int c;
+
+    c = next();
+    while (c == ' ' || c == '\t' || c == '\n'|| c == '\r' || c == '\f') {
+        c = next();
+    }
+    return (c);
+}
+
+
+static int scanint(int c) {
+    int k, val = 0;
+
+    while ((k = chrpos("0123456789", c)) >= 0) {
+        val = val * 10 + k;
+        c = next();
+    }
+
+    putback(c);
+    return val;
+}
+
+
+int scan(struct Token *t) {
+    int c;
+
+    c = skip_whitespace();
+
+    switch(c) {
+        case EOF:
+            return (0);
+        case '+':
+            t->token = T_PLUS;
+            break;
+        case '-':
+            t->token = T_MINUS;
+            break;
+        case '*':
+            t->token = T_STAR;
+            break;
+        case '/':
+            t->token = T_SLASH;
+            break;
+        default:
+            // If it's a digit, scan the
+            // literal inteteger value in
+            if (isdigit(c)) {
+                t->intvalue = scanint(c);
+                t->token = T_INTLIT;
+                break;
+            }
+
+            printf("Unrecognized character %c on line %d\n", c, Line);
+            exit(1);
+    }
+
+    // We found a token
+    return (1);
+}
